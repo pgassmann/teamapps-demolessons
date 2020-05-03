3d3f4d6acaa5db2b6712b9d27e535638c148f53b
Philipp Gassmann <gassmann@puzzle.ch>
2020-05-03 23:06:14 +0200

    update IssueTrackerApp, refactor, add more Comments

--- src/main/java/org/teamapps/demolessons/p4_issuetracker/IssueTableModel.java
+++ src/main/java/org/teamapps/demolessons/p4_issuetracker/IssueTableModel.java
@@ -4,14 +4,18 @@ import org.teamapps.data.value.SortDirection;
 import org.teamapps.data.value.Sorting;
 import org.teamapps.demolessons.issuetracker.model.issuetrackerdb.Issue;
 import org.teamapps.demolessons.issuetracker.model.issuetrackerdb.IssueQuery;
+import org.teamapps.universaldb.index.text.TextFilter;
 import org.teamapps.ux.component.table.AbstractTableModel;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public class IssueTableModel extends AbstractTableModel<Issue> {
     private String fullTextSearchString;
+    private Map<String, String> textFilterByFieldName = new HashMap<>();
 
     @Override
     public int getCount() {
@@ -35,16 +39,35 @@ public class IssueTableModel extends AbstractTableModel<Issue> {
     private IssueQuery createQuery() {
         IssueQuery issueQuery = Issue.filter();
 
+        // Filters on Fields
+        textFilterByFieldName.forEach((fieldName, searchString) -> {
+            issueQuery.fullTextFilter(TextFilter.termContainsFilter(searchString), fieldName);
+        });
+
         // FullTextFilter
         if (isNotBlank(fullTextSearchString)) {
+            // parseFullTextFilter searches every field by default.
+            // parseFullTextFilter has extra features with some syntax.
+            // E.g. Add + after a term to enable fuzzy matching
             issueQuery.parseFullTextFilter(fullTextSearchString);
         }
 
         return issueQuery;
     }
 
+    // Called by the global Search Field in the Table Panel's RightHeaderField
     public void setFullTextFilter(String searchString) {
         this.fullTextSearchString = searchString;
+        onAllDataChanged.fire();
+    }
+
+    // Called by the Filter Fields in the HeaderRow of a column.
+    public void setTextFilter(String fieldName, String searchString) {
+        if (isNotBlank(searchString)) {
+            textFilterByFieldName.put(fieldName, searchString);
+        } else {
+            textFilterByFieldName.remove(fieldName);
+        }
         onAllDataChanged.fire();
     }
 
--- src/main/java/org/teamapps/demolessons/p4_issuetracker/IssueTrackerApp.java
+++ src/main/java/org/teamapps/demolessons/p4_issuetracker/IssueTrackerApp.java
@@ -13,9 +13,10 @@ import org.teamapps.ux.application.layout.StandardLayout;
 import org.teamapps.ux.application.perspective.Perspective;
 import org.teamapps.ux.application.view.View;
 import org.teamapps.ux.component.Component;
-import org.teamapps.ux.component.dummy.DummyComponent;
+import org.teamapps.ux.component.absolutelayout.Length;
 import org.teamapps.ux.component.field.AbstractField;
 import org.teamapps.ux.component.field.Fields;
+import org.teamapps.ux.component.field.MultiLineTextField;
 import org.teamapps.ux.component.field.TextField;
 import org.teamapps.ux.component.form.ResponsiveForm;
 import org.teamapps.ux.component.form.ResponsiveFormLayout;
@@ -29,17 +30,18 @@ import org.teamapps.webcontroller.SimpleWebController;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class IssueTrackerApp implements DemoLesson {
 
-    private Component rootComponent = new DummyComponent();
+    private Component rootComponent;
     private SessionContext context;
 
     private TextField typeField;
     private TextField priorityField;
     private TextField stateField;
     private TextField summaryField;
-    private TextField descriptionField;
+    private MultiLineTextField descriptionField;
     private TextField assignedToField;
     private TextField reporterField;
     private List<AbstractField<?>> formFields;
@@ -56,19 +58,15 @@ public class IssueTrackerApp implements DemoLesson {
         this.rootComponent = createUI();
     }
 
-    private void createDemoData() {
-        if (Issue.getCount() == 0) {
-            Issue.create()
-                    .setSummary("VerticalLayout is not Scrollable")
-                    .save();
-            Issue.create()
-                    .setSummary("Finish implementation of issue tracker")
-                    .save();
-            Issue.create()
-                    .setSummary("WorkspaceLayout: Remove Panel")
-                    .save();
-        }
+    // Method called from DemoLessons App
+    @Override
+    public void handleDemoSelected() {
     }
+
+    public Component getRootComponent() {
+        return rootComponent;
+    }
+
 
     private static void startDb() {
         File storagePath = new File("./server-data/db-storage");
@@ -82,49 +80,157 @@ public class IssueTrackerApp implements DemoLesson {
         }
     }
 
-    @Override
-    public void handleDemoSelected() {
+    private void createDemoData() {
+        if (Issue.getCount() == 0) {
+            Issue.create()
+                    .setType("BUG")
+                    .setSummary("VerticalLayout scroll")
+                    .setDescription("VerticalLayout is not Scrollable")
+                    .save();
+            Issue.create()
+                    .setType("TODO")
+                    .setPriority("HIGH")
+                    .setState("NEW")
+                    .setSummary("issueTracker")
+                    .setDescription("Finish implementation of issue tracker")
+                    .save();
+            Issue.create()
+                    .setType("FEATURE")
+                    .setPriority("LOW")
+                    .setSummary("WorkspaceLayout: Remove Panel")
+                    .setDescription("This feature is missing")
+                    .save();
+            Issue.create()
+                    .setType("FEATURE")
+                    .setPriority("LOW")
+                    .setSummary("IssueTracker: Use Enums")
+                    .setDescription("Type, Priority should use ENUMs")
+                    .save();
+            Issue.create()
+                    .setType("FEATURE")
+                    .setPriority("MEDIUM")
+                    .setSummary("IssueTracker: References")
+                    .setDescription("Assignee, Reporter")
+                    .save();
+            Issue.create()
+                    .setType("FEATURE")
+                    .setPriority("MEDIUM")
+                    .setState("DONE")
+                    .setSummary("IssueTracker: MultiLine")
+                    .setDescription("MultiLine Field for Description")
+                    .save();
+        }
     }
 
-    public Component getRootComponent() {
-        return rootComponent;
-    }
-
+    // Design of the IssueTracker Application
     private Component createUI() {
-        //create a responsive application that will run on desktops as well as on smart phones
+        // create a responsive application that will run on desktops as well as on smart phones
         ResponsiveApplication application = ResponsiveApplication.createApplication();
 
-        //create perspective with default layout
+        // create perspective with default layout
         Perspective perspective = Perspective.createPerspective();
         application.addPerspective(perspective);
+
+        // Issue Table with a Table Model for Query and Filtering
         issueTableModel = new IssueTableModel();
         issueTable = createIssueTable(issueTableModel);
         View tableView = View.createView(StandardLayout.CENTER, MaterialIcon.REPORT_PROBLEM, "Issue List", issueTable);
         perspective.addView(tableView);
-        tableView.getPanel().setRightHeaderField(createSearchTextField());
 
+        // Add Global Search Field to Panel
+        tableView.getPanel().setRightHeaderField(createFilterTextField(s -> issueTableModel.setFullTextFilter(s)));
+
+        // Add Issue Detail Form View
         Component issueForm = createIssueDetailForm();
-        View formView = View.createView(StandardLayout.RIGHT, MaterialIcon.REPORT_PROBLEM, "Issue Details", issueForm);
+        View formView = View.createView(StandardLayout.RIGHT, MaterialIcon.BUG_REPORT, "Issue Details", issueForm);
         formView.addLocalButtonGroup(createFormToolbarButtonGroup());
         perspective.addView(formView);
 
-        // perspective.addWorkspaceButtonGroup(issueMasterDetail.getWorkspaceButtonGroup());
+        // displayedIssue is a TwoWayBindableValue<Issue>
+        // we can add listeners to this Object that will fire every time we update the content.
+        displayedIssue.bindWritingTo(this::updateForm);
+
+//        // bindWritingTo is the short form of
+//        // update Form now
+//        updateForm(displayedIssue.get());
+//        // update form when displayedIssue changes
+//        displayedIssue.onChanged().addListener(issue -> updateForm(issue));
+
+        // When an issue is selected in the table,
+        // update displayedIssue which will call updateForm(issue)
         issueTable.onRowSelected.addListener(issue -> {
             displayedIssue.set(issue);
+            // setting focus is helper for the mobile view where only one view is shown at a time.
+            // show the form when an element is selected in the list.
             formView.focus();
         });
-
-        displayedIssue.bindWritingTo(this::updateForm);
 
         application.showPerspective(perspective);
         return application.getUi();
     }
 
-    private TextField createSearchTextField() {
-        TextField searchField = new TextField();
-        searchField.setEmptyText("Search...");
-        searchField.onTextInput.addListener(s -> issueTableModel.setFullTextFilter(s));
-        return searchField;
+    // Define Table / List of Issues in the Center view
+    private Table<Issue> createIssueTable(IssueTableModel tableModel) {
+        Table<Issue> table = new Table<>();
+        table.setModel(tableModel);
+        table.addColumn(new TableColumn<Issue>("type", "Type", new TextField()).setMaxWidth(80));
+        table.addColumn(new TableColumn<Issue>("priority", "Priority", new TextField()).setMaxWidth(80));
+        table.addColumn(new TableColumn<Issue>("state", "State", new TextField()).setMaxWidth(80));
+        table.addColumn(new TableColumn<>("summary", "Summary", new TextField()));
+        table.addColumn(new TableColumn<>("description", "Description", new TextField()));
+        table.addColumn(new TableColumn<>("assignedTo", "assigned to", new TextField()));
+        table.addColumn(new TableColumn<>("reporter", "Reporter", new TextField()));
+
+        table.setForceFitWidth(true);
+        table.setDisplayAsList(true);
+
+        // Add Filter Fields above every column.
+        // When their value changes, in the tableModel, the filter for the corresponding field is updated
+        table.setShowHeaderRow(true);
+        table.setHeaderRowField("type", createFilterTextField(searchString -> tableModel.setTextFilter("type", searchString), "..."));
+        table.setHeaderRowField("priority", createFilterTextField(searchString -> tableModel.setTextFilter("priority", searchString),"..."));
+        table.setHeaderRowField("state", createFilterTextField(searchString -> tableModel.setTextFilter("state", searchString)));
+        table.setHeaderRowField("summary", createFilterTextField(searchString -> tableModel.setTextFilter("summary", searchString)));
+        table.setHeaderRowField("description", createFilterTextField(searchString -> tableModel.setTextFilter("description", searchString)));
+
+        return table;
+    }
+
+    // Define form on the right with editable Fields
+    private ResponsiveForm<Issue> createIssueDetailForm() {
+        ResponsiveForm<Issue> form = new ResponsiveForm<>(100, 150, 0);
+        ResponsiveFormLayout formLayout = form.addResponsiveFormLayout(450);
+
+        typeField = new TextField();
+        priorityField = new TextField();
+        stateField = new TextField();
+        summaryField = new TextField();
+        descriptionField = new MultiLineTextField();
+        descriptionField.setMinHeight(Length.ofPixels(200));
+        assignedToField = new TextField();
+        reporterField = new TextField();
+
+        summaryField.setRequired(true);
+        descriptionField.setRequired(true);
+
+        formFields = Arrays.asList(
+                typeField,
+                priorityField,
+                stateField,
+                summaryField,
+                descriptionField,
+                assignedToField,
+                reporterField);
+
+        formLayout.addSection(MaterialIcon.INFO, "Issue");
+        formLayout.addLabelAndField(null, "Type", typeField);
+        formLayout.addLabelAndField(null, "Priority", priorityField);
+        formLayout.addLabelAndField(null, "State", stateField);
+        formLayout.addLabelAndField(null, "Summary", summaryField);
+        formLayout.addLabelAndField(null, "Description", descriptionField);
+        formLayout.addLabelAndField(null, "assigned to", assignedToField);
+        formLayout.addLabelAndField(null, "Reporter", reporterField);
+        return form;
     }
 
     private ToolbarButtonGroup createFormToolbarButtonGroup() {
@@ -149,9 +255,9 @@ public class IssueTrackerApp implements DemoLesson {
             deleteIssue(displayedIssue.get());
         });
         // show save and delete button only when an issue is displayed
-        displayedIssue.bindWritingTo(person -> {
-            deleteButton.setVisible(person != null);
-            saveButton.setVisible(person != null);
+        displayedIssue.bindWritingTo(issue -> {
+            deleteButton.setVisible(issue != null);
+            saveButton.setVisible(issue != null);
         });
         return group;
     }
@@ -164,6 +270,7 @@ public class IssueTrackerApp implements DemoLesson {
         }
     }
 
+    // Save values from formFields to Issue in Database
     private void saveForm(Issue issue) {
         if (issue != null && Fields.validateAll(formFields)){
             issue.setType(typeField.getValue());
@@ -177,6 +284,7 @@ public class IssueTrackerApp implements DemoLesson {
         }
     }
 
+    // Update Form field values from issue
     private void updateForm(Issue issue) {
         if (issue != null) {
             typeField.setValue(issue.getType());
@@ -196,55 +304,15 @@ public class IssueTrackerApp implements DemoLesson {
         }
     }
 
-    private Component createIssueDetailForm() {
-        ResponsiveForm<Issue> form = new ResponsiveForm<>(100, 150, 0);
-        ResponsiveFormLayout formLayout = form.addResponsiveFormLayout(450);
-
-        typeField = new TextField();
-        priorityField = new TextField();
-        stateField = new TextField();
-        summaryField = new TextField();
-        descriptionField = new TextField();
-        assignedToField = new TextField();
-        reporterField = new TextField();
-
-        summaryField.setRequired(true);
-        descriptionField.setRequired(true);
-
-        formFields = Arrays.asList(
-                typeField,
-                priorityField,
-                stateField,
-                summaryField,
-                descriptionField,
-                assignedToField,
-                reporterField);
-
-        formLayout.addSection(MaterialIcon.PERSON, "Person");
-        formLayout.addLabelAndField(null, "Type", typeField);
-        formLayout.addLabelAndField(null, "Priority", priorityField);
-        formLayout.addLabelAndField(null, "State", stateField);
-        formLayout.addLabelAndField(null, "Summary", summaryField);
-        formLayout.addLabelAndField(null, "Description", descriptionField);
-        formLayout.addLabelAndField(null, "assigned to", assignedToField);
-        formLayout.addLabelAndField(null, "Reporter", reporterField);
-        return form;
+    // FilterTextField which calls the Consumer onTextInput
+    private TextField createFilterTextField(Consumer<String> searchTextHandler, String emptyText) {
+        TextField searchField = new TextField();
+        searchField.setEmptyText(emptyText);
+        searchField.onTextInput.addListener(searchTextHandler);
+        return searchField;
     }
-
-    private Table<Issue> createIssueTable(IssueTableModel tableModel) {
-        Table<Issue> table = new Table<>();
-        table.setModel(tableModel);
-        table.addColumn(new TableColumn<>("type", "Type", new TextField()));
-        table.addColumn(new TableColumn<>("priority", "Priority", new TextField()));
-        table.addColumn(new TableColumn<>("state", "State", new TextField()));
-        table.addColumn(new TableColumn<>("summary", "Summary", new TextField()));
-        table.addColumn(new TableColumn<>("description", "Description", new TextField()));
-        table.addColumn(new TableColumn<>("assignedTo", "assigned to", new TextField()));
-        table.addColumn(new TableColumn<>("reporter", "Reporter", new TextField()));
-
-        table.setForceFitWidth(true);
-        table.setDisplayAsList(true);
-        return table;
+    private TextField createFilterTextField(Consumer<String> searchTextHandler) {
+        return createFilterTextField(searchTextHandler, "Search...");
     }
 
     public static void main(String[] args) throws Exception {
