1645ed1dc0ece6d6dd005408f98e86b79eb7f922
Philipp Gassmann <phiphi@phiphi.ch>
2021-06-16 22:28:24 +0200

    VerticalLayout - dynamically add Components

--- src/main/java/org/teamapps/demolessons/p1_intro/l03_verticallayout/VerticalLayoutDemo.java
+++ src/main/java/org/teamapps/demolessons/p1_intro/l03_verticallayout/VerticalLayoutDemo.java
@@ -1,14 +1,19 @@
 package org.teamapps.demolessons.p1_intro.l03_verticallayout;
 
+import org.teamapps.common.format.Color;
 import org.teamapps.demolessons.DemoLesson;
+import org.teamapps.icon.antu.AntuIcon;
 import org.teamapps.icon.material.MaterialIcon;
 import org.teamapps.server.jetty.embedded.TeamAppsJettyEmbeddedServer;
 import org.teamapps.ux.component.Component;
 import org.teamapps.ux.component.dummy.DummyComponent;
+import org.teamapps.ux.component.field.Button;
+import org.teamapps.ux.component.field.FieldEditingMode;
 import org.teamapps.ux.component.field.TextField;
 import org.teamapps.ux.component.flexcontainer.VerticalLayout;
 import org.teamapps.ux.component.panel.Panel;
 import org.teamapps.ux.component.rootpanel.RootPanel;
+import org.teamapps.ux.component.template.BaseTemplateRecord;
 import org.teamapps.ux.session.SessionContext;
 import org.teamapps.webcontroller.WebController;
 
@@ -36,19 +41,31 @@ public class VerticalLayoutDemo implements DemoLesson {
         panel.setStretchContent(false);
         panel.setPadding(10);
 
+        VerticalLayout verticalLayout = new VerticalLayout();
+        panel.setContent(verticalLayout);
 
         TextField textField1 = new TextField();
+        verticalLayout.addComponent(textField1);
         textField1.setEmptyText("Enter some text here...");
 
         TextField textField2 = new TextField();
+        textField2.setValue("Multiple Components can be added to VerticalLayout. Click the Button to dynamically add more");
+        verticalLayout.addComponent(textField2);
 
+        // onTextInput immediately fires when something is written. onValueChanged only fires when the user moves focus from the Field.
         textField1.onTextInput.addListener(text -> textField2.setValue("New Content: " + text));
 
+        //noinspection rawtypes
+        Button<BaseTemplateRecord> button = Button.create(AntuIcon.ACTION_JOURNAL_NEW_24, "Add new TextField with value of first TextField").setColor(Color.BEIGE);
+        button.onClicked.addListener(() -> {
+            // Create new TextField when Button is clicked and add it to verticalLayout
+            TextField newTextField = new TextField();
+            newTextField.setValue(textField1.getValue());
+            newTextField.setEditingMode(FieldEditingMode.DISABLED);
+            verticalLayout.addComponent(newTextField);
+        });
+        verticalLayout.addComponent(button);
 
-        VerticalLayout verticalLayout = new VerticalLayout();
-        verticalLayout.addComponent(textField1);
-        verticalLayout.addComponent(textField2);
-        panel.setContent(verticalLayout);
     }
 
 
